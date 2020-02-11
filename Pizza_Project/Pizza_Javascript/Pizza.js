949a3a133ae7c1b755405ab1d91dd680c5e827f6
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-11 11:10:03 -0500

    made changes to html

--- Pizza_Project/Pizza_Javascript/Pizza.js
+++ Pizza_Project/Pizza_Javascript/Pizza.js
@@ -10,27 +10,29 @@ function getReceipt() {
 			var selectedSize = sizeArray[i].value;
 			text1 = text1+selectedSize+"<br>";
 		}
+	
+		if (selectedSize === "Personal Pizza") {
+			sizeTotal = 6;
+		} else if (selectedSize === "Small Pizza") {
+			sizeTotal = 8;
+		} else if (selectedSize === "Medium Pizza") {
+			sizeTotal = 10;
+		} else if (selectedSize === "Large Pizza") {
+			sizeTotal = 14;
+		} else if (selectedSize === "Extra Large Pizza") {
+			sizeTotal = 16;
+		}
 	}
-	if (selectedSize === "Personal Pizza") {
-		sizeTotal = 6;
-	} else if (selectedSize === "Small Pizza") {
-		sizeTotal = 8;
-	} else if (selectedSize === "Medium Pizza") {
-		sizeTotal = 10;
-	} else if (selectedSize === "Large Pizza") {
-		sizeTotal = 14;
-	} else if (selectedSize === "Extra Large Pizza") {
-		sizeTotal = 16;
-	}
+
 	runningTotal = sizeTotal;
 	console.log(selectedSize+" = $"+sizeTotal+".00");
 	console.log("size text1: "+text1);
 	console.log("subtotal: $"+runningTotal+".00");
-	getTopping(runningTotal,text1); // All three of these variables will be passed on to each function
-};
+	getTopping(runningTotal, text1); // All three of these variables will be passed on to each function
+}
 
 
-function getTopping(runningTotal,text1) {
+function getTopping(runningTotal, text1) {
 	var toppingTotal = 0;
 	var selectedTopping = [];
 	var toppingArray = document.getElementsByClassName("toppings");
@@ -54,36 +56,7 @@ function getTopping(runningTotal,text1) {
 	console.log("Purchase Total: "+"$"+runningTotal+".00");
 	document.getElementById("showText").innerHTML=text1;
 	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
-};	
+}
 
 
-// function getMeat(runningTotal, text1) {
-//     var meatTotal = 0;
-//     var selectedMeat = [];
-//     var meatArray = document.getElementsByClassName("meats");
-//     for (var j = 0; j < meatArray.length; j++) {
-//         if (meatArray[j].checked) {
-//             selectedMeat.push(meatArray[j].value);
-//             console.log("Selected meat item: ("+meatArray[j].value+")");
-//             text1 = text1+meatArray[j].value+"<br>";
-//         }
-//     }
-
-//     var meatCount = selectedMeat.length;
-//     if (meatCount > 1) {
-//         meatTotal = (meatCount - 1);
-//     } else {
-//         meatTotal = 0;
-//     }
-//     runningTotal = (runningTotal + meatTotal);
-
-
-
-//     console.log("total selected meat items: "+meatCount);
-//     console.log(meatCount+"meat - 1 free meat = "+"$"+meatTotal+".00");
-//     console.log("meat text1: "+text1);
-//     console.log("Purchase Total: "+"$"+runningTotal+".00");
-//     document.getElementById("showText").innerHTML=text1;
-//     document.getElementById("totalPrice").innerHTML="<h3>Total: <strong>$"+runningTotal+".00"+"</strong>";
-// }
 
